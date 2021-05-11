c79abe1037fec50a13431ac61d48a6c98c1a2375
Ben Chitty <htmlgxn@protonmail.com>
2021-05-10 23:34:31 -0300

    Added a Switch element; route is now /:user instead of /card/:user

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,14 +8,14 @@ const Contact = (props) => {
                 className='ui raised very padded text container segment'
                 style={{marginTop: '80px'}}
             >
-                <Link to='/card/alex' className='ui header'>Contact</Link>
+                <Link to='/alex' className='ui header'>Alex</Link>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, corporis odit sapiente quibusdam voluptates voluptatibus porro eos, ipsum omnis distinctio tempore? Debitis soluta deleniti minima illum ut consectetur adipisci quam. Mollitia quibusdam doloremque ea reprehenderit. Quas consequuntur voluptate consequatur dolore?</p>
             </div>
             <div
                 className='ui raised very padded text container segment'
                 style={{marginTop: '80px'}}
             >
-                <Link to='/card/ben' className='ui header'>Contact</Link>
+                <Link to='/ben' className='ui header'>Ben</Link>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, corporis odit sapiente quibusdam voluptates voluptatibus porro eos, ipsum omnis distinctio tempore? Debitis soluta deleniti minima illum ut consectetur adipisci quam. Mollitia quibusdam doloremque ea reprehenderit. Quas consequuntur voluptate consequatur dolore?</p>
             </div>
         </div>
