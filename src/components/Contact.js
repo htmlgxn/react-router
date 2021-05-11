7c401712117090717ab84222d12f80de452f1957
Ben Chitty <htmlgxn@protonmail.com>
2021-05-10 22:29:42 -0300

    Added Contact and About components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Home = () => {
+const Contact = () => {
     return (
         <div
             className='ui raised very padded text container segment'
             style={{marginTop: '80px'}}
         >
-            <h3 className='ui header'>Home</h3>
+            <h3 className='ui header'>Contact</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, corporis odit sapiente quibusdam voluptates voluptatibus porro eos, ipsum omnis distinctio tempore? Debitis soluta deleniti minima illum ut consectetur adipisci quam. Mollitia quibusdam doloremque ea reprehenderit. Quas consequuntur voluptate consequatur dolore?</p>
         </div>
     )
 }
 
-export default Home;
+export default Contact;
